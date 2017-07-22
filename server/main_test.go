cf1657fc9bb55238f55a572f138057dd0c730865
Merrick Kavolsky <technicmk@gmail.com>
2017-07-22 20:38:48 +0200

    Write product update test

--- server/main_test.go
+++ server/main_test.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"testing"
 
+	"gopkg.in/mgo.v2/bson"
+
 	"github.com/stretchr/testify/assert"
 
 	db "./database"
@@ -27,26 +29,48 @@ const (
 	jsonContentType    = "application/json"
 )
 
-// TestMain wraps all tests with the needed initialized mock DB and fixtures
 func TestMain(m *testing.M) {
-	// Set up
 	crud = db.NewCRUD(nil)
-
-	// Run the test suite
 	retCode := m.Run()
-
-	// Clean up
 	crud.Close()
 
-	// call with result of m.Run()
 	os.Exit(retCode)
 }
 
-func Test_NewProductEndpoint(t *testing.T) {
-	route := routing.Routes["NewProduct"]
-	h := http.HandlerFunc(route.Maker(crud))
-	handler := mware.ApplyMiddleware(h, route.Middleware)
+//==================================
+// HELPER FUNCTIONS
+//==================================
 
+func generateProductFixture(n int) []models.Product {
+	products := make([]models.Product, n)
+	for i := 0; i < n; i++ {
+		products[i] = models.Product{
+			ID:       bson.NewObjectId(),
+			Name:     "Item " + string(i),
+			Brand:    "Brand",
+			Category: "Stuff",
+			Quantity: 20,
+			Sell:     55.5,
+			Buy:      35,
+		}
+	}
+
+	return products
+}
+
+//prepareHandler prepares a handler which matches the same name
+//from the Routes map
+func prepareHandler(name string) http.Handler {
+	route := routing.Routes[name]
+	handler := mware.ApplyMiddleware(http.HandlerFunc(route.Maker(crud)), route.Middleware)
+	return handler
+}
+
+//==================================
+// TEST HANDLERS
+//==================================
+func Test_NewProductHandler(t *testing.T) {
+	handler := prepareHandler("NewProduct")
 	prod := models.Product{
 		Name:     "Yuka Socks",
 		Brand:    "Magic Feet",
@@ -55,20 +79,67 @@ func Test_NewProductEndpoint(t *testing.T) {
 		Sell:     55.5,
 		Buy:      35,
 	}
-
 	prodJSON, _ := json.Marshal(prod)
-	req := httptest.NewRequest("POST", route.Path, bytes.NewBuffer(prodJSON))
 
+	req := httptest.NewRequest("POST", "/", bytes.NewBuffer(prodJSON))
 	req.Header.Add("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 	handler.ServeHTTP(w, req)
-	resp := w.Result()
-	body, _ := ioutil.ReadAll(resp.Body)
-	var respProd models.Product
+	res := w.Result()
+	body, _ := ioutil.ReadAll(res.Body)
 
+	var respProd models.Product
 	_ = json.Unmarshal(body, &respProd)
+
 	assert := assert.New(t)
-	assert.Equal(http.StatusCreated, resp.StatusCode, invalidStatusCode)
-	assert.Equal(jsonContentType, resp.Header.Get("Content-Type"), invalidContentType)
+	assert.Equal(http.StatusCreated, res.StatusCode, invalidStatusCode)
+	assert.Equal(jsonContentType, res.Header.Get("Content-Type"), invalidContentType)
 	assert.True(prod.Equals(respProd), unexpectedResponse)
+}
+
+func Test_AllProductsHandler(t *testing.T) {
+	handler := prepareHandler("AllProducts")
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+	res := w.Result()
+
+	assert := assert.New(t)
+	assert.Equal(http.StatusOK, res.StatusCode, invalidStatusCode)
+	assert.Equal(jsonContentType, res.Header.Get("Content-Type"), invalidContentType)
+}
+
+func Test_UpdateProductsHandler(t *testing.T) {
+	products := generateProductFixture(10)
+	crud.Insert("products", products)
+	ts := httptest.NewServer(routing.NewRouter(crud))
+	defer ts.Close()
+
+	id := products[0].ID
+	url := ts.URL + "/products/" + id.Hex()
+	updateProd := models.Product{
+		ID:       id,
+		Name:     "Yuka Socks",
+		Brand:    "Magic Feet",
+		Category: "Footwear",
+		Quantity: 20,
+		Sell:     55.5,
+		Buy:      35,
+	}
+	payload, _ := json.Marshal(updateProd)
+
+	req, _ := http.NewRequest("PUT", url, bytes.NewBuffer(payload))
+	req.Header.Add("content-type", "application/json")
+
+	res, _ := http.DefaultClient.Do(req)
+	assert := assert.New(t)
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var respProd models.Product
+	_ = json.Unmarshal(body, &respProd)
+
+	assert.Equal(http.StatusOK, res.StatusCode, invalidStatusCode)
+	assert.Equal(jsonContentType, res.Header.Get("Content-Type"), invalidContentType)
+	assert.True(updateProd.Equals(respProd), unexpectedResponse)
 }
