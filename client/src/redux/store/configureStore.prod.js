01fe2929dc9a8b086cc0bc164ef92c7f39b876cf
Merrick Kavolsky <technicmk@gmail.com>
2017-07-27 22:05:40 +0200

    Fix bugs in production redux store

--- client/src/redux/store/configureStore.prod.js
+++ client/src/redux/store/configureStore.prod.js
@@ -1,27 +1,17 @@
 import {
 	createStore,
-	applyMiddleware,
-	combineReducers
+	applyMiddleware
 } from 'redux'
 import thunk from 'redux-thunk'
-import {
-	routerMiddleware,
-	routerReducer
-} from 'react-router-redux'
+'react-router-redux'
 
-import {history} from '../../config'
 import rootReducer from '../reducers'
 
 
-const middleware = routerMiddleware(history)
-
 const configureStore = preloadedState => createStore(
-	combineReducers({
-		...rootReducer,
-		router: routerReducer
-	}),
+	rootReducer,
 	preloadedState,
-	applyMiddleware(middleware, thunk)
+	applyMiddleware(thunk)
 )
 
 export default configureStore
