2cecfd1426b85bbfb5167cb90f9556fff1cd2fb6
Merrick Kavolsky <technicmk@gmail.com>
2017-08-14 14:27:13 +0200

    Add new createProduct sync action and update sync reducer

--- client/src/redux/actions/sync.js
+++ client/src/redux/actions/sync.js
@@ -1,6 +1,15 @@
+import axios from 'axios'
 import {actionType} from '../reducers/sync'
-import {snProducts, snUpdateTimes, apiUrl} from '../../config'
-import {clearObjectStore, insert, fetchAll} from '../../db'
+import {
+	snProducts,
+	snUpdateTimes,
+	apiUrl
+} from '../../config'
+import {
+	clearObjectStore,
+	 insert,
+	 fetchAll
+} from '../../db'
 
 /**
  * Decide whether or not to synchronize.
@@ -15,21 +24,42 @@ const doSync = (now, lastUpdate) => {
 	
 	//get time in seconds
 	const dt = (now.time - lastUpdate.time) / 1000
-	const tenMinutes = 10 * 60
+	const tenMinutes = 5//10 * 60
 	
 	//return true if dt is greater than 10 minutes
 	return (dt > tenMinutes)
 }
 
-export const syncWithExternalDb = () => (dispatch) =>{
+export const createProduct = (product, fn) => (dispatch) => {
+	dispatch({type: actionType.ADD_NEW_PRODUCT_PENDING})
+	const onsuccess = () => {
+		dispatch({
+			type: actionType.ADD_NEW_PRODUCT_FULFILLED,
+			payload: product
+		})
+		if (fn) fn()
+	}
+	const onerror = (err) => dispatch({
+		type: actionType.SYNC_REJECTED,
+		payload: err
+	})
+
+	insert(snProducts, [product], onsuccess, onerror)
+}
+
+export const syncWithExternalDb = (createdProducts) => (dispatch) =>{
 	const onsuccess = (times) => {
 		const now = {time: new Date().getTime()}
 		//determine whether to synchronize
 		if (!doSync(now, times.slice(-1)[0])) return
 		
-		
+		console.log('synchronizing')
 		dispatch({type: actionType.SYNC_PENDING})
-		syncProducts(dispatch)
+		const onerror = (err) => dispatch({
+			type: actionType.SYNC_REJECTED,
+			payload: err
+		})
+		syncHelper(createdProducts, onerror)
 		insert(snUpdateTimes, [now])
 		dispatch({type: actionType.SYNC_FULFILLED})
 	}
@@ -40,42 +70,32 @@ export const clearError = () => (dispatch) => dispatch({
 	type: actionType.CLEAR_ERROR
 })
 
-const syncProducts = (dispatch) => {
-	const onerror = (err) => dispatch({
-		type: actionType.SYNC_REJECTED,
-		payload: err
-	})
-
-	fetch(`${apiUrl}/products/all`)
-		.then(response => response.json())
-		.then(products => {
-			console.log('data =>', products)
+const syncHelper = (createdProducts, onerror) => {
+	createdProducts = (createdProducts)? createdProducts: []
+	axios.post(`${apiUrl}/sync`, createdProducts)
+	  .then((products) => {
 			const onsuccess = () => {
-				console.log('successfully cleared')
-				insert(snProducts, products, null, onerror)
+				insert(snProducts, products.data, null, onerror)
 			}
 			clearObjectStore(snProducts, onsuccess, onerror)
 		})
-		.catch(onerror)
+	  .catch(onerror)
+
+	//   fetch(`${apiUrl}/sync`, {
+	// 	method: 'post',
+	// 	headers: {
+	// 		'Accept': 'application/json',
+	// 		'Content-Type': 'text/plain'
+	// 	},
+	// 	body: JSON.stringify(createdProducts)
+	// })
+	// 	.then((response) => response.json())
+	// 	.then((products) => {
+	// 		console.log('products =>', products)
+	// 		const onsuccess = () => {
+	// 			insert(snProducts, products, null, onerror)
+	// 		}
+	// 		clearObjectStore(snProducts, onsuccess, onerror)
+	// 	})
+	// 	.catch(onerror)
 }
-
-//async version
-// const syncProducts = async () => (dispatch) => {
-// 	dispatch({type: actionType.SYNC_PENDING})
-// 	const onerror = (err) => dispatch({
-// 		type: actionType.SYNC_REJECTED,
-// 		payload: err
-// 	})
-
-// 	try{
-// 		const response = await fetch(`${apiUrl}/products/all`)
-// 		const products = await response.json()
-		
-// 		const onInsertSuccess = () => {
-// 			insert(snProducts, products, null, onerror)
-// 		}
-// 		clearObjectStore(snProducts, onInsertSuccess, onerror)
-// 	}catch(err){
-// 		onerror(err)
-// 	}
-// }
--- client/src/redux/reducers/sync.js
+++ client/src/redux/reducers/sync.js
@@ -14,6 +14,10 @@ export const actionType = {
 	SYNC_FULFILLED: 'SYNC_FULFILLED',
 	SYNC_REJECTED: 'SYNC_REJECTED',
 
+	ADD_NEW_PRODUCT_PENDING: 'ADD_NEW_PRODUCT',
+	ADD_NEW_PRODUCT_FULFILLED: 'ADD_NEW_PRODUCT_FULFILLED',
+	ADD_NEW_PRODUCT_REJECTED: 'ADD_NEW_PRODUCT_REJECTED',
+
 	CLEAR_ERROR: 'CLEAR_SYNC_ERROR'
 }
 
@@ -24,6 +28,10 @@ export default (state=initialState, action) => {
 			return state.set('pending', true)
 
 		case actionType.SYNC_FULFILLED:
+			//don't do anything if there was an error, i.e. don't clear
+			//the stored data
+			if (state.get('error'))
+				return state
 			return state.merge({
 				pending: false,
 				products: Set()
@@ -35,6 +43,23 @@ export default (state=initialState, action) => {
 				error: action.payload
 			})
 
+
+		//Adding product
+		case actionType.ADD_NEW_PRODUCT_PENDING:
+			return state.set('pending', true)
+
+		case actionType.ADD_NEW_PRODUCT_FULFILLED:
+			return state.merge({
+				pending: false,
+				products: state.get('products').add(action.payload)
+			})
+
+		case actionType.ADD_NEW_PRODUCT_REJECTED:
+			return state.merge({
+				pending: false,
+				error: action.payload
+			})
+		
 		case actionType.CLEAR_ERROR:
 			return state.set('error', null)
 	}
